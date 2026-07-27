1fd7f263508806b186d33003697a7fb956fb18e4
Matias Santillan <sntlln.93@gmail.com>
2026-07-26 23:01:52 -0300

    feat(seeders): add patients seeder for dev/test data

--- apps/api/database/seeders/DatabaseSeeder.php
+++ apps/api/database/seeders/DatabaseSeeder.php
@@ -38,5 +38,6 @@ class DatabaseSeeder extends Seeder
         $this->call(InsuranceProviderSeeder::class);
         $this->call(CatalogSeeder::class);
         $this->callWith(ProfessionalsSeeder::class, ['organization' => $organization]);
+        $this->callWith(PatientsSeeder::class, ['organization' => $organization]);
     }
 }
