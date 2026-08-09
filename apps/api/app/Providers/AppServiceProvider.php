e16ce5073715fbe5767a7bb3ffea945a978bc047
Matias Santillan <sntlln.93@gmail.com>
2026-08-09 03:26:28 -0300

    style(providers): reduce comment verbosity

--- apps/api/app/Providers/AppServiceProvider.php
+++ apps/api/app/Providers/AppServiceProvider.php
@@ -9,19 +9,10 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         $this->app->singleton(CurrentOrganization::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+    public function boot(): void {}
 }
